2cfb50f3372092dcdcb2fbb99aae129e9c0a6981
aka-lucifer <34045997+aka-lucifer@users.noreply.github.com>
2018-05-19 16:29:29 +0100

    Add files via upload

---
+++ client.lua
@@ -0,0 +1,57 @@
+local holstered = true
+
+local weapons = {
+	"WEAPON_PISTOL",
+	"WEAPON_COMBATPISTOL",
+}
+
+ 
+ Citizen.CreateThread(function()
+	while true do
+		Citizen.Wait(0)
+		local ped = PlayerPedId()
+		if DoesEntityExist( ped ) and not IsEntityDead( ped ) and not IsPedInAnyVehicle(PlayerPedId(), true) then
+			loadAnimDict( "reaction@intimidation@1h" )
+			if CheckWeapon(ped) then
+				if holstered then
+					TaskPlayAnim(ped, "reaction@intimidation@1h", "outro", 8.0, 2.0, -1, 48, 10, 0, 0, 0 )
+					Citizen.Wait(600)
+					ClearPedTasks(ped)
+					holstered = false
+				end
+				SetPedComponentVariation(ped, 9, 0, 0, 0)
+			elseif not CheckWeapon(ped) then
+				if not holstered then
+					TaskPlayAnim(ped, "reaction@intimidation@1h", "intro", 8.0, 2.0, -1, 48, 10, 0, 0, 0 )
+					Citizen.Wait(500)
+					ClearPedTasks(ped)
+					holstered = true
+				end
+				SetPedComponentVariation(ped, 9, 1, 0, 0)
+			end
+		end
+	end
+end)
+function CheckWeapon(ped)
+	for i = 1, #weapons do
+		if GetHashKey(weapons[i]) == GetSelectedPedWeapon(ped) then
+			return true
+		end
+	end
+	return false
+end
+--[[
+function DisableActions(ped)
+	DisableControlAction(1, 140, true)
+	DisableControlAction(1, 141, true)
+	DisableControlAction(1, 142, true)
+	DisableControlAction(1, 37, true) -- Disables INPUT_SELECT_WEAPON (TAB)
+	DisablePlayerFiring(ped, true) -- Disable weapon firing
+end
+--]]
+function loadAnimDict( dict )
+	while ( not HasAnimDictLoaded( dict ) ) do
+		RequestAnimDict( dict )
+		Citizen.Wait( 0 )
+	end
+end
